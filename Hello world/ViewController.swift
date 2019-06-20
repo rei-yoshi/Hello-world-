d54a51d492780817f8facf4e649d4cb2422183b5
rei-yoshi <rei.yosida.5a@stu.hosei.ac.jp>
2019-06-20 20:53:42 +0900

    Initial Commit

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by 吉田れい on 2019/06/20.
+//  Copyright © 2019 吉田れい. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
---
+++ Hello worldTests/Hello_worldTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hello_worldTests.swift
+//  Hello worldTests
+//
+//  Created by 吉田れい on 2019/06/20.
+//  Copyright © 2019 吉田れい. All rights reserved.
+//
+
+import XCTest
+@testable import Hello_world
+
+class Hello_worldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
